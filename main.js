5431dcbb8e619407f5fca748f0ece08eda940ba9
andyling1106 <69571636+andyling1106@users.noreply.github.com>
2020-08-22 08:14:52 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -61,6 +61,27 @@ var onComplete = function(error) {
   }
 };
 
+//function that gets the location and returns it
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geo Location not supported by browser");
+  }
+}
+//function that retrieves the position
+function showPosition(position) {
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  console.log(location)
+}
+//request for location
+getLocation();
+
+
+
 
 //Save messages to firebase
 function saveMessage(passno,name,cdate){
